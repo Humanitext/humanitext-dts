6059cc9d0c4dc91896b31deb31c3c0c5c088734c
junjun7613 <htjk6513khbk@gmail.com>
2025-04-20 21:52:07 +0900

    eit index

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,6 @@ app.get("/", (req: Request, res: Response) => {
   res.redirect("/api/dts");
 });
 
-app.get('/', (req, res, next) => {
-  res.set({ 'Access-Control-Allow-Origin': '*' }); // ここでヘッダーにアクセス許可の情報を追加
-  
-  // 何らかの処理
-});
-
 app.use("/api/dts", dtsRouter);
 app.use("/api/dts/document", documentRouter);
 app.use("/api/dts/navigation", navigationRouter);
